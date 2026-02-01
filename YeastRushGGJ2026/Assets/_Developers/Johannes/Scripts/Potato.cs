fd1d18963e6da10821606b02d592cc6f94dee00c
Johannes Tuemler <johannes.tuemler@hs-anhalt.de>
2026-02-01 10:47:43 +0100

    Johannes: Added Fart Bubble

--- YeastRushGGJ2026/Assets/_Developers/Johannes/Scripts/Potato.cs
+++ YeastRushGGJ2026/Assets/_Developers/Johannes/Scripts/Potato.cs
@@ -1,7 +1,6 @@
 using System.Runtime.CompilerServices;
 using Unity.Mathematics;
 using Unity.VisualScripting;
-using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 
 public class Potato : MonoBehaviour
